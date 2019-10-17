df4874feebf085b58fdc9d2d3c275753ae2e7199
MomeDesktop\MomePP <peeranut32@gmail.com>
2019-10-18 00:05:28 +0700

    added updating indicator & UI adjust

--- src/main/index.js
+++ src/main/index.js
@@ -5,80 +5,77 @@ import { app, BrowserWindow, ipcMain, dialog } from 'electron'
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-    global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
 const winURL = process.env.NODE_ENV === 'development'
-    ? `http://localhost:9080`
-    : `file://${__dirname}/index.html`
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
 
 function createWindow() {
-    /**
-     * Initial window options
-     */
-    mainWindow = new BrowserWindow({
-        height: 400,
-        useContentSize: true,
-        width: 720
-    })
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 360,
+    useContentSize: true,
+    width: 640
+  })
+  mainWindow.setMenu(null)
+  mainWindow.loadURL(winURL)
 
-    mainWindow.loadURL(winURL)
-
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow()
-    }
+  if (mainWindow === null) {
+    createWindow()
+  }
 })
 
 ipcMain.on('browse-firmware-path', () => {
-    var firmwarePath = dialog.showOpenDialog({ properties: ['openFile'], title: 'select new firmware to update' })
+  var firmwarePath = dialog.showOpenDialog({ properties: ['openFile'], title: 'select new firmware to update' })
 
-    if (firmwarePath != null) {
-        mainWindow.webContents.send('browse-firmware-path-response', firmwarePath)
-    }
+  if (firmwarePath != null) {
+    mainWindow.webContents.send('browse-firmware-path-response', firmwarePath)
+  }
 })
 
 ipcMain.on("update-firmware", (event, arg) => {
-    var child = require('child_process').execFile;
-    var executablePath = ''
-    var parameters = [arg];
+  var child = require('child_process').execFile;
+  var executablePath = ''
+  var parameters = [arg];
 
-    console.log(require('os').platform())
+  if (require('os').platform() == 'win32') {
+    executablePath = "src\\script\\windows\\hid-flash.exe";
+  }
+  else if (require('os').platform() == 'darwin') {
+    executablePath = "src\\script\\darwin\\hid-flash";
+  }
 
-    if (require('os').platform() == 'win32')
-    {
-        executablePath = "src\\script\\windows\\hid-flash.exe";
+  mainWindow.webContents.send('update-firmware-start')
+
+  child(executablePath, parameters, function (err, data) {
+    if (err) {
+      // may show error dialog ...
+      console.log(err)
+      return;
     }
-    else if (require('os').platform() == 'darwin')
-    {
-        executablePath = "src\\script\\darwin\\hid-flash";
-    }
 
-    // mainWindow.webContents.send('update-firmware-start')
-
-    child(executablePath, parameters, function(err, data) {
-        if(err){
-        //    mainWindow.webContents.send('update-firmware-error', err)
-           return;
-        }
-     
-        console.log(data.toString());
-        // mainWindow.webContents.send('update-firmware-success')
-    });
+    console.log(data.toString());
+    mainWindow.webContents.send('update-firmware-success')
+  });
 })
 
 /**
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,59 +1,78 @@
 <template>
-    <div id="wrapper">
-        <img id="logo" src="~@/assets/gogo_logo.png" alt="electron-vue" />
-        <main>
-            <div class="left-side">
-                <div class="doc">
-                    <p>{{ firmware_path }}</p>
-                    <button class="alt" @click="openDialog()">Browse firmware file</button>
-                    <br />
-                    <br />
-                </div>
-                <div class="doc">
-                    <button @click="updateFirmware()">Update</button>
-                </div>
-            </div>
-        </main>
-    </div>
+  <div id="wrapper">
+    <img id="logo" src="~@/assets/gogo_logo.png" alt="electron-vue" />
+    <main>
+      <div class="left-side">
+        <div class="doc">
+          <p>{{ firmware_path }}</p>
+        </div>
+      </div>
+      <div class="right-side">
+        <button @click="openDialog()">Browse firmware file</button>
+      </div>
+    </main>
+    <main>
+      <div></div>
+      <div class="update">
+        <br />
+        <br />
+        <br />
+        <span class="loader" v-if="isUpdating">
+          <clip-loader :loading="loading" :color="color" :size="size"></clip-loader>
+        </span>
+        <span class="loader" v-else></span>
+        <button class="alt" @click="updateFirmware()">Update</button>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
+import ClipLoader from "vue-spinner/src/ClipLoader.vue";
+
 export default {
-    name: "landing-page",
-    methods: {
-        open(link) {
-            this.$electron.shell.openExternal(link);
-        },
-        openDialog() {
-            this.$electron.ipcRenderer.send("browse-firmware-path");
-
-            this.$electron.ipcRenderer.on(
-                "browse-firmware-path-response",
-                (event, arg) => {
-                    this.firmware_path = arg[0];
-                }
-            );
-        },
-        updateFirmware() {
-			if (this.firmware_path != null && this.firmware_path != 'browse for firmware path')
-				this.$electron.ipcRenderer.send("update-firmware", this.firmware_path);
-
-			// this.$electron.ipcRenderer.on('update-firmware-start', (event, arg) => {
-				
-			// })
-			// this.$electron.ipcRenderer.on('update-firmware-error', (event, arg) => {
-
-			// })
-			// this.$electron.ipcRenderer.on('update-firmware-success', () => {
-
-			// })
-		}
+  name: "landing-page",
+  components: {
+    ClipLoader
+  },
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link);
     },
-    data() {
-        return {
-            firmware_path: "browse for firmware path"
-        };
+    openDialog() {
+      this.$electron.ipcRenderer.send("browse-firmware-path");
+
+      this.$electron.ipcRenderer.on(
+        "browse-firmware-path-response",
+        (event, arg) => {
+          this.firmware_path = arg[0];
+        }
+      );
+    },
+    updateFirmware() {
+      if (
+        this.firmware_path != null &&
+        this.firmware_path != "browse for firmware path"
+      ) {
+        this.$electron.ipcRenderer.send("update-firmware", this.firmware_path);
+      }
+
+      this.$electron.ipcRenderer.on("update-firmware-start", () => {
+        this.isUpdating = true;
+      });
+      this.$electron.ipcRenderer.on("update-firmware-success", () => {
+        this.isUpdating = false;
+      });
     }
+  },
+  data() {
+    return {
+      firmware_path: "browse for firmware path",
+      color: "#051e3e ",
+      size: "25px",
+      isUpdating: false
+    };
+  }
 };
 </script>
 
@@ -61,85 +80,84 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
 
 * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
 body {
-    font-family: "Source Sans Pro", sans-serif;
+  font-family: "Source Sans Pro", sans-serif;
 }
 
 #wrapper {
-    background: radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, 0.9) 100%
-    );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
+  background: radial-gradient(
+    ellipse at top left,
+    rgba(255, 255, 255, 1) 40%,
+    rgba(229, 229, 229, 0.9) 100%
+  );
+  height: 100vh;
+  padding: 60px 80px;
+  width: 100vw;
 }
 
 #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 320px;
+  height: auto;
+  margin-bottom: 50px;
+  width: 320px;
 }
 
 main {
-    display: flex;
-    justify-content: space-between;
-}
-
-main > div {
-    flex-basis: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
 }
 
 .left-side {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
-.welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
+.right-side {
+  display: flex;
 }
 
-.title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
+.break {
+  flex-basis: 100%;
+  height: 40px;
 }
 
-.title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
+.update {
+  display: flex;
+  align-items: center;
+}
+
+.loader {
+  margin-top: 5px;
+  margin-right: 10px;
 }
 
 .doc p {
-    color: black;
-    margin-bottom: 10px;
+  color: black;
+  margin-bottom: 10px;
+  margin-right: 10px;
 }
 
-.doc button {
-    font-size: 0.8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
+main button {
+  font-size: 0.8em;
+  cursor: pointer;
+  outline: none;
+  padding: 0.75em 2em;
+  border-radius: 2em;
+  display: inline-block;
+  color: #651e3e ;
+  background-color: transparent;
+  transition: all 0.15s ease;
+  box-sizing: border-box;
+  border: 1px solid #651e3e ;
 }
 
-.doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+main button.alt {
+  color: #fff;
+  background-color: #851e3e ;
 }
 </style>
